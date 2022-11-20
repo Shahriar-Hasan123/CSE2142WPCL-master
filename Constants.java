80ea64f13415600ebf3acafa272a0424d0d2792d
Shahriar Hasan <shahriar19735@gmail.com>
2022-11-21 01:55:13 +0600

    Final improvement

--- Constants.java
+++ Constants.java
@@ -1,23 +1,23 @@
 public class Constants {
-    public static final String Students_File_Name = "students.txt";
-    public static final String WORDS_SPLIT_REGEX = ",";
-    public static final String Date_Format_Pattern = "dd-mm-yyyy hh:mm:ss a";
-    public static final int Invalid_Arg_exit_status = 1;
 
+    public static final String StudentFileName="students.txt";
+    public static final String WordSplitRegex=", ";
+    public static final String DataFormatePattern="dd/mm/yyyy-hh:mm:ss a";
+    public static final int InvalidArgStatus=1;
 
-    public static final String List_of_Data = "a";
-    public static final String Random_Data = "r";
-    public static final String Add_Data = "+";
-    public static final String Find_Data = "?";
-    public static final String Count_Words = "c";
+    public static final String ListOfData="a";
+    public static final String RandomData="r";
+    public static final String FindData="?";
+    public static final String AddData="+";
+    public static final String CountData="c";
 
-
-    public static final String Invalid_Msg = "Invalid arguments.";
-    public static final String Loading_Data_Msg = "Loading data ...";
-    public static final String Loaded_Data_Msg = "Data Loaded.";
-    public static final String Updated_Data_Msg = "\nList last updated on ";
-    public static final String Word_Founded_Data_Msg = " word(s) found ";
-    public static final String Data_Found_Msg = "We found it!";
+    public static final String InvalidMsg="Invalid number of argument";
+    public static final String ExitMsg="Exiting programme";
+    public static final String LoadingMsg="Loading data ...";
+    public static final String LoadedMsg="Data Loaded.";
+    public static final String UpdatedMsg="\nList last updated on ";
+    public static final String DataFoundMsg="We found it!";
+    public static final String WordFoundMsg=" word(s) found ";
 
 
 
--- StudentList.java
+++ StudentList.java
@@ -2,97 +2,77 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
-
-	public static String getLineFromFile() throws Exception {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(Constants.Students_File_Name)));
+	public static String readFile() throws Exception{
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(Constants.StudentFileName)));
 		return bufferedReader.readLine();
 	}
-
-	public static BufferedWriter getFileBufferedWriter() throws Exception {
-       return new BufferedWriter(new FileWriter(Constants.Students_File_Name, true));
+	public static BufferedWriter writeFile()throws Exception{
+		return new BufferedWriter(new FileWriter(Constants.StudentFileName, true));
 	}
-
 	public static void main(String[] args) {
-
-//		Check arguments
-		if(args[0].equals(Constants.List_of_Data)) {
-			System.out.println(Constants.Loading_Data_Msg);
-			try {
-			       for(String name : getLineFromFile().split(Constants.WORDS_SPLIT_REGEX)) {
-				      System.out.println(name);
-			       }
-			} catch (Exception e){
-
-			}
-			System.out.println(Constants.Loaded_Data_Msg);
+		//		Check Validity of arguments
+		if(args.length!=1){
+			System.err.println(Constants.InvalidMsg);
+			System.err.println(Constants.ExitMsg);
+			System.exit(Constants.InvalidArgStatus);
 		}
-		else if(args[0].equals(Constants.Random_Data)) {
-			System.out.println(Constants.Loading_Data_Msg);
+		//		Check arguments
+		if(args[0].equals(Constants.ListOfData)) { //List data arguments.
+			System.out.println(Constants.LoadingMsg);
 			try {
-			      System.out.println(getLineFromFile());
-		     	  String names[] =getLineFromFile().split(Constants.WORDS_SPLIT_REGEX);
-				  System.out.println(names[new Random().nextInt()]);
-			} catch (Exception e){
-
-			}
-			System.out.println(Constants.Loaded_Data_Msg);
+				for(String  index: readFile().split(Constants.WordSplitRegex)) {
+					System.out.println(index);
+				}
+			} catch (Exception e){}
+			System.out.println(Constants.LoadedMsg);
 		}
-		else if(args[0].contains(Constants.Add_Data)) {
-			System.out.println(Constants.Loading_Data_Msg);
+		else if(args[0].equals(Constants.RandomData)) {//Show random data arguments.
+			System.out.println(Constants.LoadingMsg);
 			try {
-			     BufferedWriter bufferedWriter = getFileBufferedWriter();
-	             DateFormat dateFormat = new SimpleDateFormat(Constants.Date_Format_Pattern);
-	             String formatdate= dateFormat.format(new Date());
-			     bufferedWriter.write(Constants.WORDS_SPLIT_REGEX+" "+args[0].substring(Constants.Invalid_Arg_exit_status)+Constants.Updated_Data_Msg+formatdate);
-			     bufferedWriter.close();
-			} catch (Exception e){
-
-			}
-							
-			System.out.println(Constants.Loaded_Data_Msg);
+				System.out.println(readFile());
+				String newReadLine[] = readFile().split(Constants.WordSplitRegex);
+				System.out.println(newReadLine[new Random().nextInt()]);
+			} catch (Exception e){}
+			System.out.println(Constants.LoadedMsg);
 		}
-		else if(args[0].contains(Constants.Find_Data)) {
-			System.out.println(Constants.Loading_Data_Msg);
+		else if(args[0].contains(Constants.AddData)){//Add data arguments.
+			System.out.println(Constants.LoadingMsg);
 			try {
-
-			      String names[] = getLineFromFile().split(Constants.WORDS_SPLIT_REGEX);
-			      boolean done = false;
-			      for(int idx = 0; idx<names.length && !done; idx++) {
-				    if(names[idx].equals(args[0].substring(1))) {
-					  System.out.println(Constants.Data_Found_Msg);
-						done=true;
-				    }
-				  }
-			} catch (Exception e){
-
-			}
-			System.out.println(Constants.Loaded_Data_Msg);
+				String formatDate= new SimpleDateFormat(Constants.DataFormatePattern).format(new Date());
+				writeFile().write(Constants.WordSplitRegex+args[0].substring(Constants.InvalidArgStatus)+Constants.UpdatedMsg+formatDate);
+				writeFile().close();
+			} catch (Exception e){}
+			System.out.println(Constants.LoadedMsg);
 		}
-		else if(args[0].contains(Constants.Count_Words))
+		else if(args[0].contains(Constants.FindData)) {//Find data arguments.
+			System.out.println(Constants.LoadingMsg);
+			try {
+				if(Arrays.asList(readFile().split(Constants.WordSplitRegex)).contains(args[0].substring(1))) {
+					System.out.println(Constants.DataFoundMsg);
+				}
+			}
+			catch (Exception e){}
+			System.out.println(Constants.LoadedMsg);
+		}
+		else if(args[0].contains(Constants.CountData)) {//Count data arguments.
+			System.out.println(Constants.LoadingMsg);
+			try {
+				char charRead[] = readFile().toCharArray();
+				int count=0;
+				for(char index:charRead) {
+					if(index ==' ')
+						count=count+1;
+				}
+				System.out.println((count / 2 + count % 2) + Constants.WordFoundMsg + charRead.length);
+			} catch (Exception e) {}
+			System.out.println(Constants.LoadedMsg);
+		}
+		else
 		{
-			System.out.println(Constants.Loading_Data_Msg);
-			try {
-			      boolean in_word = false;
-			      int count=0;
-			      for(char word:getLineFromFile().toCharArray()) {
-				    if(word ==' '){
-					   if (!in_word) {
-						   count++;
-						   in_word =true;
-					   }
-					   else {
-						   in_word=false;
-					   }
-				    }
-			      }
-			      System.out.println(count +Constants.Word_Founded_Data_Msg + getLineFromFile().toCharArray().length);
-			} catch (Exception e){
-
-			}
-			System.out.println(Constants.Loaded_Data_Msg);
-		}
-		else{
-			System.out.println(Constants.Invalid_Msg);
+			System.err.println(Constants.InvalidMsg);
+			System.err.println(Constants.ExitMsg);
+			System.exit(Constants.InvalidArgStatus);
 		}
 	}
 }
